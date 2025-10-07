913945e16c9b0da23e35fb33cdccf76f70a3831f
Ahmed Hemdan <aha007@edu.zealand.dk>
2025-10-07 10:35:30 +0200

    dd

--- LaundryLibrary/Model/Apartment.cs
+++ LaundryLibrary/Model/Apartment.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Data.SqlClient;
+
 
 // Class responsible for representing address and apartment identity for a resident
 
